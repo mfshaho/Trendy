811d211c78842398a5a4cb62cc50370767f16e34
Mohammed Khattab Ismail <abojaso23@gmail.com>
2022-10-24 21:03:31 -0800

    Ninwa

--- en/product/buy.js
+++ en/product/buy.js
@@ -1,20 +1,28 @@
-$(document).ready(function(){
-    $('#selectModel').change(function(){
-        if($(this).data('options') == undefined){
-            $(this).data('options', $('#selectStorage option').clone());
-        }
-        var id = $(this).val();
-        var options = $(this).data('options').filter('[data-value=' + id + ']');
-        $('#selectStorage').html(options).show();
+var model={
+    iphone14:['128GB', '256GB', '512GB'],
+    iphone14pro:['128GB', '256GB', '512GB', '1TB'],
+    iphone14promax:['128GB', '256GB', '512GB', '1TB']
+}
+
+var main= document.getElementById('main_menu');
+var sub= document.getElementById('sub_menu');
+
+main.addEventListener('change', function (){
+
+    var selected_option = model[this.value];
+
+    while(sub.options.length > 0){
+
+        sub.options.remove(0);
+
+    }
+
+    Array.from(selected_option).forEach(function (el){
+
+        let option = new Option(el, el);
+
+        sub.appendChild(option);
+
     });
 
-    $('#selectStorage').change(function(){
-        if($(this).data('options') == undefined){
-            $(this).data('options', $('#selectColor option').clone());
-        }
-        var id = $(this).val();
-        var options = $(this).data('options').filter('[data-value=' + id + ']');
-        $('#selectColor').html(options).show();
-    });
 });
-
