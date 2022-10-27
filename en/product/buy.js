08a01646c0fb885a7b1b56178b21cd65c9b66b61
Mohammed Khattab Ismail <abojaso23@gmail.com>
2022-10-27 21:21:12 +0300

    checkout

--- en/product/buy.js
+++ en/product/buy.js
@@ -1,4 +1,34 @@
+/* Total Price */
+$(document).ready(function () {
+    let model = 0, size = 0;
+    $('input[name="model"]').on("click", function () {
+        model = parseInt($('input[name="model"]:checked').val());
 
+    });
+
+    $('input[name="size"]').on("click", function () {
+        size = parseInt($('input[name="size"]:checked').val());
+    });
+
+    $(".form-selector-input").on('click', function () {
+        let sum = model + size;
+        $("#item-price").html("$" + sum + ".00");
+    });
+});
+
+/* Shipping Fee */
+// iPhone 14 Price //
+var basePrice = 0;
+
+$(".calculate").change(function() {
+    newPrice = basePrice;
+    $(".calculate option:selected").each(function() {
+        newPrice += $(this).data('price')
+    });
+    $("#shipping-fee").html(newPrice);
+});
+
+/* Hide/Show Elements */
 function next1() {
     var radio = document.getElementById("model");
     var text = document.getElementById("color");
